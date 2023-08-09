54189474e8376a5179004753a5f257e407f295f1
unknown <dandarabiancas@gmail.com>
2023-08-09 16:44:44 -0300

    formulario

--- src/Assets/Components/ReviewForm.jsx
+++ src/Assets/Components/ReviewForm.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./ReviewForm.css";
+
 
 export default function ReviewForm() {
   return (
@@ -13,6 +15,37 @@ export default function ReviewForm() {
         />
         <span>Insatisfeito</span>
       </label>
+      <label className='radio-conteiner'>
+        <input
+        type='radio'
+        value='neutral'
+        name='review'
+        required
+        />
+        <span>Poderia ser melhor</span>
+      </label>
+      <label className='radio-conteiner'>
+        <input
+        type='radio'
+        value='satisfied'
+        name='review'
+        required
+        />
+        <span>Satisfeito</span>
+      </label>
+      <label className='radio-conteiner'>
+        <input
+        type='radio'
+        value='very_satisfied'
+        name='review'
+        required
+        />
+        <span>Muito satisfeito</span>
+      </label>
+     </div>
+     <div className='form-control'>
+     <label htmlFor='coment'>Cometário:</label>
+     <textarea name='coment' id='coment' placeholder='Conte como foi a sua experiencia com o produto...' required></textarea>
      </div>
     </div>
   )
